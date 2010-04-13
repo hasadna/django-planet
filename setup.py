c54961a3c4c41e016ce160f971bb5a64c9c369b7
Milton Mazzarri <milmazz@gmail.com>
2010-04-13 01:46:57 -0430

    Adding setup.py (stereosteve)

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(
+    name='django-planet',
+    version=version,
+    description="A django app to create a rss planet",
+    url='http://github.com/stereosteve/django-planet',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=['setuptools_git'],
+)
